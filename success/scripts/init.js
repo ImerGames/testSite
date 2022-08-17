cd37f5e074c9d458fd120584ae9b80049c9b9fc5
ZiroxCh <40709454+ImerGames@users.noreply.github.com>
2022-08-17 22:07:44 +0300

    Add files via upload

--- success/scripts/init.js
+++ success/scripts/init.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-    $('.containerModules').masonry({horizontalOrder: true});
+    let settingsColumn = 3;
+    let waterfall = new Waterfall({ 
+        containerSelector: '.containerModules',
+        boxSelector: '.module',
+        minBoxWidth: 250
+    });
     $("#input_phone_callback").inputmask({"mask": "+38 (999) 999-99-99"});
     $("#input_email_callback").inputmask("email");
+    
+    // $(window).resize(function(e){
+    //     let ScreenWidth = screen.width;     
+    //     if(ScreenHeight <= 1000){
+    //         $('.containerModules').masonryGrid({'columns': 2});
+    //     } 
+    // });
 });
--- success/scripts/script.js
+++ success/scripts/script.js
@@ -1,25 +1,36 @@
 $(document).ready(function(){
+    let mouseOffset = { x : "110" , y : "110" }
+    let targetOffset = { x : "0" , y : "0" } 
     $("#btn_open_close_more_modules").click(function(){
         let isOpen = $(this).attr("data-open");
         
         if(isOpen == "true"){
             //open
-            for(let i = 0 ; i < $(".secondOrderModules").length; i++){
-                $(".secondOrderModules").eq(i).show(300);
-            }
-            setTimeout(function(){
-                $('.containerModules').masonry({horizontalOrder: true});
-            },500);
+            $(".secondOrderModules").show(300);
             $(this).attr("data-open" , "false");
             $(this).text("Менше модулів");
+            i=0;
+            let animOpen = setInterval( function(){
+                $(".containerModules>div>div.secondOrderModules").eq(i).css({"opacity":"1"});
+                if(i == $(".secondOrderModules").length){
+                    clearInterval(animOpen);
+                }
+                i++;
+            }, 400);
         }else{
             //close
-            for(let i = 0 ; i < $(".secondOrderModules").length; i++){
-                $(".secondOrderModules").eq(i).hide(300);
-            }
+            i=$(".secondOrderModules").length -1;
+            let animClose = setInterval( function(){
+                $(".containerModules>div>div.secondOrderModules").eq(i).css({"opacity":"0"});
+                if(i == 0){
+                    clearInterval(animClose);
+                }
+                i--;
+            }, 400);
+            let time = 400 * ($(".secondOrderModules").length +1);
             setTimeout(function(){
-                $('.containerModules').masonry({horizontalOrder: true});
-            },300);
+                $(".secondOrderModules").hide(300);
+            },time);
             $(this).attr("data-open" , "true");
             $(this).text("Більше модулів");
         }
